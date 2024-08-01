b626b966447de4dfb4657a58f3caad53d3537369
equalsales@gmail.com <equalsales@gmail.com>
2024-08-01 16:09:06 +0530

    kavita

--- lib/dashboard/sidebar.dart
+++ lib/dashboard/sidebar.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_mobile/module/looms/beamcard/beamcardlist.dart';
 import 'package:cloud_mobile/module/looms/customreport/customreportmenu.dart';
+
 import 'package:cloud_mobile/module/looms/jobreceive/yarnjobworkreceive/list_yarnjobworkreceive.dart';
+
+import 'package:cloud_mobile/module/looms/jobreceive/beamjobworkreceive/list_beamjobworkreceive.dart';
 import 'package:cloud_mobile/module/looms/machinecard/loomsmachinecardlist.dart';
 import 'package:cloud_mobile/module/looms/physicalstock/loomphysicalstocklist.dart';
 import 'package:cloud_mobile/module/looms/purchasechallan/purchasemenu.dart';
@@ -307,21 +310,21 @@ class SideDrawer extends StatelessWidget {
                           )))
             },
           ),
-          ListTile(
-            leading: Icon(Icons.shopping_cart),
-            title: Text('Taka Production'),
-            onTap: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => TakaProductionList(
-                            companyid: xcompanyid,
-                            companyname: xcompanyname,
-                            fbeg: xfbeg,
-                            fend: xfend,
-                          )))
-            },
-          ),
+          // ListTile(
+          //   leading: Icon(Icons.shopping_cart),
+          //   title: Text('Taka Production'),
+          //   onTap: () => {
+          //     Navigator.push(
+          //         context,
+          //         MaterialPageRoute(
+          //             builder: (_) => TakaProductionList(
+          //                   companyid: xcompanyid,
+          //                   companyname: xcompanyname,
+          //                   fbeg: xfbeg,
+          //                   fend: xfend,
+          //                 )))
+          //   },
+          // ),
           ListTile(
             leading: Icon(Icons.shopping_cart),
             title: Text('Yarn Physical Stock Entry'),
